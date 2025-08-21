684a91574ac8cc2a58ef529847a007bf226d623d
qidianjin <qidianjin@h-partners.com>
2025-08-21 14:07:58 +0800

    first edition for jit framework

---
+++ deep_gemm_ascend/framework/csrc/python_api.cpp
@@ -0,0 +1,22 @@
+#include <pybind11/pybind11.h>
+#include <torch/python.h>
+
+#include "jit_kernels/impls/gemm.hpp"
+
+#ifndef TORCH_EXTENSION_NAME
+#define TORCH_EXTENSION_NAME deep_gemm_cpp
+#endif
+
+namespace deep_gemm_ascend {
+void run_mmad_custom(const at::Tensor &x, const at::Tensor &y, at::Tensor &z)
+{
+    mmad_custom(x, y, z);
+}
+}
+
+// ReSharper disable once CppParameterMayBeConstPtrOrRef
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.doc() = "DeepGEMM C++ library";
+
+    m.def("run_mmad_custom", &deep_gemm_ascend::run_mmad_custom, "");
+}
