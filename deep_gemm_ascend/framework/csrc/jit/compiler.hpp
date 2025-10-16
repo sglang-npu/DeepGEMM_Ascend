bf35426599bbabde25e8cd0003fc4387dc4682d6
silencejade <qidianjin@h-partners.com>
2025-10-16 10:13:17 +0800

    add cache framework

---
+++ deep_gemm_ascend/framework/csrc/jit/compiler.hpp
@@ -0,0 +1,121 @@
+#pragma once
+
+#include <string>
+#include "cache.hpp"
+#include "../utils/system.hpp"
+#include "../utils/format.hpp"
+#include "../utils/constant.hpp"
+
+namespace deep_gemm_ascend {
+class Compiler {
+    std::string rootPath_;
+    std::string socVersion_;
+public:
+    struct CompileArgs {
+        uint32_t m, n, k;
+        uint32_t kernelType;
+    };
+    Compiler() {
+        rootPath_ = get_env<std::string>("DGA_ROOT_DIR");
+        socVersion_ = "Ascend910B3";
+    }
+
+    virtual ~Compiler() = default;
+
+    std::shared_ptr<KernelRuntime> build(const std::string& code,
+        const CompileArgs& compile_args, const std::string& kernel_name) const 
+    {
+        // 1 get from cache
+        const auto kernel_signature = fmt::format("m{}n{}k{}_type{}",
+            compile_args.m, compile_args.n, compile_args.k, compile_args.kernelType);
+        std::string kernel_dir = fmt::format("{}/deep_gemm_ascend/cache/kernel_{}/", rootPath_, kernel_signature);
+        if (const auto& runtime = kernel_runtime_cache->get(kernel_dir); runtime != nullptr) {
+            // std::cout << "use kernel cache" << std::endl;
+            return runtime;
+        }
+        // std::cout << "compile new kernel" << std::endl;
+
+        // 2 compile new cache
+        // 2.1 put code to code path
+        std::filesystem::create_directories(kernel_dir);
+        std::filesystem::path code_path = kernel_dir + utils::KERNEL_CODE_NAME;
+        OutputKernelFile(code, code_path);
+
+        // 2.2 compile code
+        compile(kernel_dir);
+
+        // 3 create runtime
+        const auto& runtime = kernel_runtime_cache->get(kernel_dir);
+        // todo: assert runtime not be nullptr
+        DGA_HOST_ASSERT(runtime != nullptr);
+        return runtime;
+    }
+
+    virtual void compile(const std::string& kernel_dir) const = 0;
+private:
+    void OutputKernelFile(const std::string& code, const std::filesystem::path& code_path) const
+    {
+        // put code to code path
+        // std::cout << "put code to code path: " << code_path << std::endl;
+        std::ofstream ofs(code_path);
+        ofs << code;
+        ofs.close();
+    }
+};
+
+class CMakeCompiler final: public Compiler {
+public:
+    CMakeCompiler() {
+        rootPath_ = get_env<std::string>("DGA_ROOT_DIR");
+        socVersion_ = "Ascend910B3";
+    }
+
+    void compile(const std::string& kernel_dir) const override {
+        std::string cmake_dir = rootPath_ + "/deep_gemm_ascend/cache/";
+        std::string buildPath = kernel_dir + "/build/";
+
+        // 1. compile kernel code
+        std::string command = "cmake " + cmake_dir +
+            " -B " + buildPath +
+            " -DSOC_VERSION=" + socVersion_ +
+            " -DKERNEL_SRC_PATH=" + kernel_dir + utils::KERNEL_CODE_NAME +
+            " && cmake --build " + buildPath;
+        // std::cout << "run cmake command: " << command << std::endl;
+        const auto& [return_code, output] = call_external_command(command);
+        // std::cout << "run cmake command result: " << return_code << std::endl;
+        // std::cout << "run cmake command output: " << output << std::endl;
+    
+        // 2. copy kernel bin to code directory
+        std::string bin_path = cmake_dir + "/out/fatbin/mmad_kernels/" + utils::KERNEL_FATBIN_NAME;
+        CopyBinFile(bin_path, kernel_dir);
+        RemoveOriginBinDir(cmake_dir);
+        // std::cout << "bin path : " << bin_path.c_str() << std::endl;
+    }
+
+    ~CMakeCompiler() = default;
+private:
+    void CopyBinFile(const std::string& bin_path, const std::string& kernel_dir) const
+    {
+        std::string command = "cp -f " + bin_path + " " + kernel_dir;
+        // std::cout << "run move command: " << command << std::endl;
+        const auto& [return_code, output] = call_external_command(command);
+        // std::cout << "run move command result: " << return_code << std::endl;
+    }
+
+    void RemoveOriginBinDir(const std::string& cmake_dir) const
+    {
+        if (cmake_dir.empty()) {
+            return;
+        }
+        std::string command = "rm -rf " + cmake_dir + "/out";
+        // std::cout << "run remove command: " << command << std::endl;
+        const auto& [return_code, output] = call_external_command(command);
+        // std::cout << "run remove command result: " << return_code << std::endl;
+    }
+
+    std::string rootPath_;
+    std::string socVersion_;
+};
+
+static auto compiler = std::make_shared<CMakeCompiler>();
+} // namespace deep_gemm_ascend
---
+++ deep_gemm_ascend/framework/csrc/jit/kernel_runtime.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "handle.hpp"
+#include "../utils/system.hpp"
+#include "../utils/constant.hpp"
+
+namespace deep_gemm_ascend {
+class KernelRuntime final {
+public:
+    LibraryHandle binHandle = nullptr;
+    KernelHandle kernel = nullptr;
+
+    explicit KernelRuntime(const std::string& kernel_dir) {
+        // todo 1 check param
+        // todo 2 parse param
+        std::string bin_path = kernel_dir + utils::KERNEL_FATBIN_NAME;
+        // 3 load file
+        kernel = load_kernel(bin_path.c_str(), "mmad_custom", &binHandle);
+    }
+
+    void ConstructArgs(LaunchArgsHandle &argsHandle, LaunchParamHandle &paramHandle, 
+        const at::Tensor &x, const at::Tensor &y, at::Tensor &z)
+    {
+        construct_launch_args(kernel, argsHandle, paramHandle, x, y, z);
+    }
+
+    static bool check_validity(const std::filesystem::path& kernel_dir) {
+        return std::filesystem::exists(kernel_dir / utils::KERNEL_CODE_NAME) and
+               std::filesystem::exists(kernel_dir / utils::KERNEL_FATBIN_NAME);
+    }
+
+    ~KernelRuntime() noexcept(false) {
+        unload_library(binHandle);
+    }
+};
+
+template <typename Derived>
+class LaunchRuntime {
+public:
+    template <typename Args>
+    static std::string generate(const Args& args) {
+        const auto& code = Derived::generate_impl(args);
+        return code;
+    }
+
+    // template <typename Args>
+    // static void launch(const std::shared_ptr<KernelRuntime>& kernel_runtime, const Args& args) {
+        // const auto& kernel = kernel_runtime->kernel;
+        // const auto& stream = at::cuda::getCurrentCUDAStream();
+        // const LaunchArgs& launch_args = args.launch_args;
+
+        // const dim3& grid_dim = {static_cast<unsigned>(launch_args.grid_dim.first),
+        //                         static_cast<unsigned>(launch_args.grid_dim.second),
+        //                         1};
+        // const dim3& block_dim = {static_cast<unsigned>(launch_args.num_threads), 1, 1};
+        // auto config = construct_launch_config(kernel, stream, launch_args.smem_size,
+        //                                       grid_dim, block_dim, launch_args.cluster_dim);
+
+        // // Launch in the derived class
+        // if (get_env<int>("DG_JIT_DEBUG")) {
+        //     printf("Launch kernel with {%d, %d} x %d, shared memory: %d bytes, cluster: %d, stream: %ld\n",
+        //            launch_args.grid_dim.first, launch_args.grid_dim.second, launch_args.num_threads,
+        //            launch_args.smem_size, launch_args.cluster_dim, stream.id());
+        // }
+        // Derived::launch_impl(kernel, config, args);
+    // }
+};
+
+
+} // namespace deep_gemm_ascend
