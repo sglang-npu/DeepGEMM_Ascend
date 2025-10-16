c31f28e13a42c3b44a3103728375192ed2cff119
lt <liutao491@huawei.com>
2025-10-16 10:34:01 +0800

    add ERROR_LOG, CHECK_ACL macro function

---
+++ deep_gemm_ascend/benchmark_msprof/benchmark_util.h
@@ -0,0 +1,28 @@
+#ifndef BENCHMARK_UTILS_H
+#define BENCHMARK_UTILS_H
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <cassert>
+#include <cstdio>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "acl/acl.h"
+#include "../framework/csrc/jit/get_best_config.hpp"
+
+#define ERROR_LOG(fmt, args...) fprintf(stdout, "[ERROR]  " fmt "\n", ##args)
+#define CHECK_ACL(x)                                                                        \
+    do {                                                                                    \
+        aclError __ret = x;                                                                 \
+        if (__ret != ACL_ERROR_NONE) {                                                      \
+            std::cerr << __FILE__ << ":" << __LINE__ << " aclError:" << __ret << std::endl; \
+        }                                                                                   \
+    } while (0);
+
+
