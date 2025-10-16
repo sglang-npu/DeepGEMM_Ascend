29239c2368d156995e0d3e279544e55c41835541
lt <liutao491@huawei.com>
2025-10-16 11:26:27 +0800

    add get_best_config && align16

--- deep_gemm_ascend/framework/tests/bench_main.py
+++ deep_gemm_ascend/framework/tests/bench_main.py
@@ -68,3 +68,57 @@ def verify_result(output, golden):
     error_ratio = float(diff_ele_idxs.size) / golden.size
     print("error ratio: %.6f, tolerance: %.4f" % (error_ratio, error_tol))
     return error_ratio <= error_tol
+
+def align16(x):
+    return (x + 15) & ~15
+
+def get_best_config(m, n, k, m_sections, n_sections, m_sec_o_blocks, n_sec_o_blocks, k_o_iter_blocks, db_o_blocks):
+    param_dic = {}
+    param_dic["m_sections"] = m_sections
+    param_dic["n_sections"] = n_sections
+    param_dic["m_sec_o_blocks"] = m_sec_o_blocks
+    param_dic["n_sec_o_blocks"] = n_sec_o_blocks
+    param_dic["k_o_iter_blocks"] = k_o_iter_blocks
+    param_dic["db_o_blocks"] = db_o_blocks
+
+    param_dic["m_blocks"] = align16(m) // 16
+    param_dic["n_blocks"] = align16(n) // 16
+    param_dic["k_blocks"] = align16(k) // 16
+    param_dic["m_o_fix"] = align16(m) - m
+    param_dic["n_o_fix"] = align16(n) - n
+    param_dic["k_o_fix"] = align16(k) - k
+
+    param_dic["db_o_num"] = k_o_iter_blocks // db_o_blocks
+
+    param_dic["r_m_blocks"] = param_dic["m_blocks"] % m_sec_o_blocks
+    param_dic["r_n_blocks"] = param_dic["n_blocks"] % n_sec_o_blocks
+    if param_dic["r_m_blocks"] == 0:
+        param_dic["r_m_blocks"] = m_sec_o_blocks
+    if param_dic["r_n_blocks"] == 0:
+        param_dic["r_n_blocks"] = n_sec_o_blocks
+
+    param_dic["k_iters"] = math.ceil(param_dic["k_blocks"] / k_o_iter_blocks)
+
+    k_tail_blocks = param_dic["k_blocks"] % k_o_iter_blocks
+    if k_tail_blocks == 0:
+        param_dic["r_db_num"] = param_dic["db_o_num"]
+        param_dic["r_k_blocks"] = db_o_blocks
+    else:
+        param_dic["r_db_num"] = math.ceil(k_tail_blocks / db_o_blocks)
+        param_dic["r_k_blocks"] = k_tail_blocks - ((param_dic["r_db_num"] - 1) * db_o_blocks)
+
+    m_iters = math.ceil(param_dic["m_blocks"] / m_sec_o_blocks)
+    n_iters = math.ceil(param_dic["n_blocks"] / n_sec_o_blocks)
+    param_dic["m_parts"] = math.ceil(m_iters / param_dic["m_sections"])
+    param_dic["n_parts"] = math.ceil(n_iters / param_dic["n_sections"])
+
+    param_dic["m_sc_blocks"] = param_dic["m_parts"] * m_sec_o_blocks
+    param_dic["n_sc_blocks"] = param_dic["n_parts"] * n_sec_o_blocks
+    param_dic["r_m_parts"] = m_iters - ((m_sections - 1) * param_dic["m_parts"] )
+    param_dic["r_n_parts"] = n_iters - ((n_sections - 1) * param_dic["n_parts"] )
+
+    param_dic["batch"] = 1
+    param_dic["m"] = m
+    param_dic["n"] = n
+    param_dic["k"] = k
+    print(f"{param_dic=}")
