5cee214355ac8b1d392d03d498e6c75468f24bc4
silencejade <qidianjin@h-partners.com>
2026-02-09 10:50:21 +0800

    add aclnn device entry point

--- aclnn_catlass_dynamic_matmul/op_kernel/catlass_dynamic_matmul.cpp
+++ aclnn_catlass_dynamic_matmul/op_kernel/catlass_dynamic_matmul.cpp
@@ -8,3 +8,38 @@
  * BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. See LICENSE in the root of
  * the software repository for the full text of the License.
  */
+
+#include "catlass_dynamic_matmul.h"
+#include "catlass_dynamic_matmul_tiling_data.h"
+#include "kernel/kernel_utils.h"
+
+template <uint32_t DATATYPE_IN, uint32_t DATATYPE_OUT, uint32_t KERNEL_TYPE,
+          uint32_t PADDING_TAGA, uint32_t PADDING_TAGB, uint32_t PADDING_TAGC, 
+          uint32_t LAYOUT_TAGA, uint32_t LAYOUT_TAGB, uint32_t LAYOUT_TAGC, 
+          uint32_t DISPATCH_POLICY_TAG>
+__global__ __aicore__
+void catlass_dynamic_matmul(GM_ADDR gmA, GM_ADDR gmB, GM_ADDR gmC, GM_ADDR workspace, GM_ADDR tiling) {
+    __gm__ uint8_t* user = AscendC::GetUserWorkspace(workspace);
+    REGISTER_TILING_DEFAULT(CatlassDynamicMatmulTilingData);
+    GET_TILING_DATA_WITH_STRUCT(CatlassDynamicMatmulTilingData, tilingData, tiling);
+
+    // half(fp16)
+    if constexpr (
+        DATATYPE_IN == static_cast<uint32_t>(CatlassDynamicMatmul::CatlassDataType::CATLASS_DT_FLOAT16) &&
+        DATATYPE_OUT == static_cast<uint32_t>(CatlassDynamicMatmul::CatlassDataType::CATLASS_DT_FLOAT16)
+    ) {
+        CatlassDynamicMatmul::DynamicMatmulKernelWrapper<half, half, KERNEL_TYPE,
+            PADDING_TAGA, PADDING_TAGB, PADDING_TAGC, LAYOUT_TAGA, LAYOUT_TAGB, LAYOUT_TAGC,
+            DISPATCH_POLICY_TAG>(gmA, gmB, gmC, user, tilingData);
+    }
+
+    // bfloat16(bf16)
+    if constexpr (
+        DATATYPE_IN == static_cast<uint32_t>(CatlassDynamicMatmul::CatlassDataType::CATLASS_DT_BF16) &&
+        DATATYPE_OUT == static_cast<uint32_t>(CatlassDynamicMatmul::CatlassDataType::CATLASS_DT_BF16)
+    ) {
+        CatlassDynamicMatmul::DynamicMatmulKernelWrapper<bfloat16_t, bfloat16_t, KERNEL_TYPE,
+            PADDING_TAGA, PADDING_TAGB, PADDING_TAGC, LAYOUT_TAGA, LAYOUT_TAGB, LAYOUT_TAGC,
+            DISPATCH_POLICY_TAG>(gmA, gmB, gmC, user, tilingData);
+    }
+}
