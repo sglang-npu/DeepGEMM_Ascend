80a2d0d122d92a4317048c51ecc8f6194b9fa43b
sigama-w <1443307488@qq.com>
2026-06-02 17:00:34 +0800

    add two layout parameters

--- get_best_config/get_best_config.py
+++ get_best_config/get_best_config.py
@@ -713,11 +713,8 @@ class GetBestConfig:
 
         self.matmul_tiling_calculator = MatmulTilingCalculator()
 
-    def predict(self, m, n, k):
+    def predict(self, m, n, k, layout_tag_a, layout_tag_b):
         shape = [m, n, k]
-        # 取layout, 不同算子的predictor的layout保持一致, 例如取small
-        layout_tag_a = self.predictor_small.catlass_param_generator.layout_tag_a
-        layout_tag_b = self.predictor_small.catlass_param_generator.layout_tag_b
         args = shape + [layout_tag_a, layout_tag_b]  # 合并参数
         # 1.计算catlass的tiling和kerneltype
         result = self.matmul_tiling_calculator.calculate(*args)
@@ -756,7 +753,7 @@ def main():
     get_best_config = GetBestConfig()
 
     # 预测任意 M、N、K 的最优 tiling 参数
-    best_tiling = get_best_config.predict(m=72, n=7392, k=8192)
+    best_tiling = get_best_config.predict(m=72, n=7392, k=8192, layout_tag_a=0, layout_tag_b=1)
     # 返回: {"m1": 16, "n1": 32, "k1": 64}
 
     if best_tiling:
